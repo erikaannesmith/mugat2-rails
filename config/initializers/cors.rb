5b240ec37233976d6a2018f17612069850ab0cfb
Erika Smith <erikaannesmith@gmail.com>
2018-03-12 18:20:28 -0600

    update cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://erikaannesmith.github.io/mugat2'
+    origins 'https://erikaannesmith.github.io'
 
     resource '*',
       headers: :any,
